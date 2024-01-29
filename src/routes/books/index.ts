c40579da3c5301ecd3310ffec1bc37e8cc8890c5
Leandro Oliveira da Silva <lleandro.silva@outlook.com>
2024-01-29 16:47:11 -0300

    atualizando dependencias

--- src/routes/books/index.ts
+++ src/routes/books/index.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
 import { BookController } from '@/controllers/BookController';
-import { authMiddleware } from '@/middlewares/authMiddleware';
 
 const booksRouter = Router();
 
-booksRouter.use(authMiddleware);
 booksRouter.post('/books/create', BookController.CreateBook);
 booksRouter.get('/books/list', BookController.ListBook);
 booksRouter.put('/books/update', BookController.UpdateBook);
--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 
 import { UserController } from '@/controllers/UserController';
-import { authMiddleware } from '@/middlewares/authMiddleware';
 
 const userRouter = Router();
 
 userRouter.post('/users/create', UserController.CreateUser);
-userRouter.put('/users/update', authMiddleware, UserController.UpdateUser);
+userRouter.put('/users/update', UserController.UpdateUser);
 userRouter.get('/users/list', UserController.ListUser);
 
 export { userRouter };
